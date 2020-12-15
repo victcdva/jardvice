c509d0931314b28b3be3acdd83e01a7c675906a9
Víctor Córdova <theslapboy@gmail.com>
2020-12-14 23:00:19 -0700

    Adding the reload page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,6 +5,7 @@
       <div class="row">
         <div class="col-md-12">
           <div class="card card-chart">
+            <h2 class="text-center">Jar Device Content</h2>
             <div class="ct-chart" id="jardevicedatachart"></div>
           </div>
         </div>
@@ -20,18 +21,16 @@
   <script src="https://code.jquery.com/jquery-3.5.1.min.js"  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
   <script type="text/javascript">
 
-    var leveldata = <?php echo $leveldata; ?>;
+    var leveldata = <?php echo empty($leveldata) ? 1 : $leveldata; ?>;
     var createdat = <?php echo $createdat;?>;
     var jardata = <?php echo $jardata;?>;
  
     var converted = JSON.stringify(jardata);
     var parsed = JSON.parse(converted);
-    
-    
-    console.log(leveldata);
-    
 
-    var gaugeOptions = {
+
+    function displayChart() {
+        var gaugeOptions = {
     chart: {
         type: 'solidgauge'
     },
@@ -117,9 +116,13 @@ var chartSpeed = Highcharts.chart('jardevicedatachart', Highcharts.merge(gaugeOp
             valueSuffix: ' km/h'
         }
     }]
-
 }));
 
+    }
+displayChart();
+setTimeout(function(){
+    location = ''
+  },6000)
 
 
 
